dd7ed3cb72aa7646b174fa45ff6a41e163c4925c
Tonyjnr <onuzulikeanthony432@gmail.com>
2025-08-11 13:36:58 +0000

    1.0.3
    Source commit: 6a48f7214ddea5115a8ea9297c1a917b14345dbc
    
    Auto-sync from private repo

--- config.default.js
+++ config.default.js
@@ -10,15 +10,15 @@ global.botVersion = "1.1.0";
 // Bot Settings
 global.prefix = "+"; // Define the prefix for commands
 global.sessionName = "lyion_session"; // Folder name to save session
-global.publicMode = true; // Set to true to allow anyone to use the bot, false for owner only
+global.publicMode = false; // Set to true to allow anyone to use the bot, false for owner only
 
 global.packname = `Lyion Bot`;
 global.author = global.ownerName;
 
 //Api Keys
-global.googleApiKey = ""; // Google API Key
-global.googleSearchEngineId = ""; // Google Custom Search Engine ID
-global.geminiApiKey = "";
+global.googleApiKey = process.env.GOOGLE_API_KEY || ""; // Google API Key
+global.googleSearchEngineId = process.env.GOOGLE_CSE_ID || ""; // Google Custom Search Engine ID
+global.geminiApiKey = process.env.GOOGLE_API_KEY || "";
 //logging settings
 global.logging = {
   level: "info", // error, warn, info, debug, trace
